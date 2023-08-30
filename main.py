a7bf4021463893b67ed9c235c51e98dccf941821
Stephan Karl <stephan.karl@newtec.de>
2023-08-30 15:11:02 +0200

    Initial commit of empty main-function

---
+++ main.py
@@ -0,0 +1,12 @@
+#!python
+
+# -- IMPORTS --------------------------------------------------------
+import pygame
+
+def main():
+    pygame.init()
+    
+    
+
+if __name__ == '__main__':
+    main()
