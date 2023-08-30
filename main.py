a85faf4d5d3f341f7494fead7c4c2c9772686df8
Stealka <s_karl@outlook.com>
2023-08-30 16:33:11 +0200

    Added first colours

--- main.py
+++ main.py
@@ -3,10 +3,22 @@
 # -- IMPORTS --------------------------------------------------------
 import pygame
 
-def main():
+# -- DEFINES --------------------------------------------------------
+BLACK = (   0,   0,   0)
+RED =   ( 255,   0,   0)
+GREEN = (   0, 255,   0)
+BLUE =  (   0,   0, 255)
+
+def init():
+    "Init function that sets up all necessary initial configurations"
+    
+    # Initializing pygame module
     pygame.init()
+    
+    
+    
     
     
 
 if __name__ == '__main__':
-    main()
+    init()
